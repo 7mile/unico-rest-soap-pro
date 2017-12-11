fa0796a0b4022fbdbc522cad3df8d2c8ce19ebab
wangxiaofei <wangxiaofei@lianjia.com>
2017-12-11 13:52:01 +1100

    update consumer

--- src/main/java/au/com/unico/message/consumer/JMSConsumer.java
+++ src/main/java/au/com/unico/message/consumer/JMSConsumer.java
@@ -1,14 +1,12 @@
 package au.com.unico.message.consumer;
 
-import au.com.unico.dao.ItemRepository;
-import au.com.unico.domain.Item;
-import au.com.unico.message.JMSConfiguration;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jms.annotation.JmsListener;
 import org.springframework.jms.core.JmsTemplate;
 import org.springframework.stereotype.Component;
+
+import javax.jms.Queue;
 
 /**
  * Created by xiaofei on 2017/12/9.
@@ -18,25 +16,20 @@ public class JMSConsumer {
     private Logger logger = LoggerFactory.getLogger(getClass());
 
     private JmsTemplate jmsTemplate;
-    private ItemRepository itemRepository;
+    private Queue queue;
 
     @Autowired
-    public JMSConsumer(JmsTemplate jmsTemplate, ItemRepository itemRepository) {
+    public JMSConsumer(JmsTemplate jmsTemplate, Queue queue) {
         this.jmsTemplate = jmsTemplate;
-        this.itemRepository = itemRepository;
+        this.queue = queue;
     }
 
-    @JmsListener(destination = JMSConfiguration.QUEUE_NAME)
-    public void processMessage(String content) {
-        logger.debug("Received message:{}", content);
-        Item item = new Item();
-        try {
-            item.setNumber(Integer.parseInt(content));
-        } catch (Exception e) {
-            logger.error("Failed to convert message:{} to Integer. Can't convert to Item. Error:", content, e.getMessage());
-            return;
+    public Object getMessage() {
+        jmsTemplate.setReceiveTimeout(500L);
+        Object msgObject = jmsTemplate.receiveAndConvert(queue);
+        if (logger.isDebugEnabled()) {
+            logger.debug("getMessage: " + msgObject);
         }
-
-        itemRepository.save(item);
+        return msgObject;
     }
 }
